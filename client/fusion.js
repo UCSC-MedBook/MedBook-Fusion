505cd040201238ae8434871fde1004c92753bb13
rbaertsch <baertsch@soe.ucsc.edu>
2015-05-01 19:59:17 -0700

    Temporary debug

--- client/fusion.js
+++ client/fusion.js
@@ -766,6 +766,8 @@ Template.Controls.rendered = function() {
 
                      if (transform.field in datum) {
                         if (transform.op == "bin") {
+                             if (transform.field == "KDM4B Mut" && datum[transform.field] != "N/A")
+                                 debugger;
                              var dataValue = parseFloat(datum[transform.field]);
                              var binValue = parseFloat(transform.value);
                              if (!isNaN(dataValue) && !isNaN(binValue)) {
