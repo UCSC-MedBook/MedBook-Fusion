b228c9e2cfb594b6b0aa8305e932cb40ae10945c
rbaertsch <baertsch@soe.ucsc.edu>
2015-05-01 20:42:44 -0700

    Cleanup

--- client/fusion.js
+++ client/fusion.js
@@ -767,8 +767,6 @@ Template.Controls.rendered = function() {
 
                      if (transform.field in datum) {
                         if (transform.op == "bin") {
-                             if (transform.field == "KDM4B Mut" && datum[transform.field] != "N/A")
-                                 debugger;
                              var dataValue = parseFloat(datum[transform.field]);
                              var binValue = parseFloat(transform.value);
                              if (!isNaN(dataValue) && !isNaN(binValue)) {
