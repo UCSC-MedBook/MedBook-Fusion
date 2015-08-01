8e73c9d08e1d7ef92ddf114117a0facbc22e4025
rbaertsch <baertsch@soe.ucsc.edu>
2015-07-31 17:45:08 -0700

    Added Top level menu

--- webapp/lib/router.js
+++ webapp/lib/router.js
@@ -21,37 +21,53 @@ Router.configure({
 
 
 
-Router.onBeforeAction('loading');
+function data() {
+    var defaultQ = {
+        post: {$exists: 0},
+        userId: Meteor.userId()
+    };
+    var data;
+
+    if (this.params._id != null) {
+        data = Charts.findOne({_id: this.params._id});
+    } else {
+        data = Charts.findOne(defaultQ);
+    }
+    return data;
+}
+
+function waitOn() {
+    return [
+      Meteor.subscribe('Chart', this.params._id),
+      Meteor.subscribe('Metadata'),
+      Meteor.subscribe('studies')
+    ]
+}
 
 Router.map(function() {
   this.route('home', {
     template: "SampleFusion",
     path: '/fusion/',
-    data: function() {
-        var defaultQ = {
-            post: {$exists: 0},
-            userId: Meteor.userId()
-        };
-        var data;
-
-        if (this.params._id != null) {
-            data = Charts.findOne({_id: this.params._id});
-            var default_obj = Charts.findOne(defaultQ);
-            data._id = default_obj._id;
-        } else {
-            data = Charts.findOne(defaultQ);
-        }
-        return data;
-    },
-
-    waitOn:  function() {
-        return [
-          Meteor.subscribe('Chart', this.params._id),
-          Meteor.subscribe('Metadata'),
-          Meteor.subscribe('studies')
-        ]
-    }
+    data: data,
+    waitOn: waitOn, 
   });
 });
 
+Router.map(function() {
+  this.route('display', {
+    template: "SampleFusion",
+    path: '/fusion/display/:_id/',
+    data: data,
+    waitOn: waitOn, 
+  });
+});
+
+Router.map(function() {
+  this.route('edit', {
+    template: "SampleFusion",
+    path: '/fusion/edit/:_id/',
+    data: data,
+    waitOn: waitOn, 
+  });
+});
 
